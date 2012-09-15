6fc180f2220af072cabf96993b734e3a08e435c4
Justo M. Casablanca <jcasablanca@bellsouth.net>
2012-09-15 16:28:54 -0400

    1) Added new methods: 	public static List<Folder> parseFolders( String js ) throws IOException, JToodleException 	private static void throwJToodleExceptionIfError( String js ) throws IOException, JToodleException
    2) Each parseXXXX method now throws a JToodleException if Toodledo web service API returns JSON string indicating an error

--- src/jtoodle/api/beans/BeanParser.java
+++ src/jtoodle/api/beans/BeanParser.java
@@ -5,19 +5,23 @@
 package jtoodle.api.beans;
 
 import com.fasterxml.jackson.core.Version;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import java.io.IOException;
 import java.util.Date;
-import jtoodle.api.json.mixin.AccountInfoMixIn;
-import jtoodle.api.json.mixin.TokenMixIn;
-import jtoodle.api.json.mixin.UserIdMixIn;
+import java.util.List;
 import jtoodle.api.json.deser.TD_BooleanDeserializer;
 import jtoodle.api.json.deser.TD_DateDeserializer;
 import jtoodle.api.json.deser.TD_DateFormatDeserializer;
 import jtoodle.api.json.deser.TD_PriorityDeserializer;
 import jtoodle.api.json.enums.DateFormat;
 import jtoodle.api.json.enums.Priority;
+import jtoodle.api.json.mixin.AccountInfoMixIn;
+import jtoodle.api.json.mixin.FolderMixIn;
+import jtoodle.api.json.mixin.JToodleExceptionMixIn;
+import jtoodle.api.json.mixin.TokenMixIn;
+import jtoodle.api.json.mixin.UserIdMixIn;
 
 /**
  *
@@ -30,9 +34,11 @@ public class BeanParser {
 	private static final ObjectMapper mapper = new ObjectMapper();
 
 	static {
+		mapper.addMixInAnnotations( JToodleException.class, JToodleExceptionMixIn.class );
 		mapper.addMixInAnnotations( UserId.class, UserIdMixIn.class );
 		mapper.addMixInAnnotations( Token.class, TokenMixIn.class );
 		mapper.addMixInAnnotations( AccountInfo.class, AccountInfoMixIn.class );
+		mapper.addMixInAnnotations( Folder.class, FolderMixIn.class );
 
 		mapper.registerModule(
 			new SimpleModule( "TD_DeserializationModule", Version.unknownVersion() )
@@ -44,15 +50,30 @@ public class BeanParser {
 
 	}
 
-	public static UserId parseUserId( String js ) throws IOException {
+	public static UserId parseUserId( String js ) throws IOException, JToodleException {
+		throwJToodleExceptionIfError( js );
 		return( mapper.readValue( js, UserId.class ) );
 	}
 
-	public static Token parseToken( String js ) throws IOException {
+	public static Token parseToken( String js ) throws IOException, JToodleException {
+		throwJToodleExceptionIfError( js );
 		return( mapper.readValue( js, Token.class ) );
 	}
 
-	public static AccountInfo parseAccountInfo( String js ) throws IOException {
+	public static AccountInfo parseAccountInfo( String js ) throws IOException, JToodleException {
+		throwJToodleExceptionIfError( js );
 		return( mapper.readValue( js, AccountInfo.class ) );
+	}
+
+	public static List<Folder> parseFolders( String js ) throws IOException, JToodleException {
+		throwJToodleExceptionIfError( js );
+		return( mapper.readValue( js, new TypeReference<List<Folder>>() {} ) );
+	}
+
+	private static void throwJToodleExceptionIfError( String js ) throws IOException, JToodleException {
+		if( js.contains( "errorCode" ) ) {
+			JToodleException jte = mapper.readValue( js, JToodleException.class );
+			throw( jte );
+		}
 	}
 }
