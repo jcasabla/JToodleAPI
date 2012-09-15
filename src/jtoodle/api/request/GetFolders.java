0d2075182309324fa353d2f4f8ede1415c8d90e9
Justo M. Casablanca <jcasablanca@bellsouth.net>
2012-09-15 16:30:30 -0400

    First check-in

---
+++ src/jtoodle/api/request/GetFolders.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jtoodle.api.request;
+
+/**
+ *
+ * @author justo
+ */
+public class GetFolders extends AbstractAPIWebRequest {
+
+	public GetFolders() {
+		super( URI_GET_FOLDERS );
+	}
+}
