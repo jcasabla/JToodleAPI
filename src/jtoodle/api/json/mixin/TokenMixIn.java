7ced1d8235501d960fd05c301ab6d04923519eb2
Justo M. Casablanca <jcasablanca@bellsouth.net>
2012-09-15 17:01:26 -0400

    BUG FIX: userId is not a property of Token

--- src/jtoodle/api/json/mixin/TokenMixIn.java
+++ src/jtoodle/api/json/mixin/TokenMixIn.java
@@ -4,15 +4,10 @@
  */
 package jtoodle.api.json.mixin;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 /**
  *
  * @author justo
  */
 public interface TokenMixIn extends AbstractJToodleBeanMixIn {
 
-	@JsonProperty( "userid" )
-	abstract String getUserId();
-	abstract void setUserId( String userId );
 }
