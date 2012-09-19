404eacaa99dba1756fbfb3e53c0ca8816fe6720c
Justo M. Casablanca <jcasablanca@bellsouth.net>
2012-09-19 06:22:13 -0400

    Added comments

--- src/jtoodle/api/json/enums/Priority.java
+++ src/jtoodle/api/json/enums/Priority.java
@@ -9,6 +9,18 @@ package jtoodle.api.json.enums;
  * @author justo
  */
 public enum Priority {
+	/*
+	 * Task Datatypes (http://api.toodledo.com/2/tasks/index.php)
+	 *
+	 * priority : An integer that represents the priority.
+	 *
+	 * -1 = Negative
+	 *  0 = Low
+	 *  1 = Medium
+	 *  2 = High
+	 *  3 = Top
+	 */
+
 	Low,
 	Medium,
 	High,
