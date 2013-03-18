b3cf82cef46ebe5e77b476c6c8308742e79798e8
Justo M. Casablanca <jcasablanca@bellsouth.net>
2013-03-18 17:52:18 -0400

    Refactor: 	- Each form tab is now separated out to its own JPanel subclass, thereby moving tab logic out of the main form 	- Common classes moved to package jtoodle.api.test.util 	- Form/GUI test classes moved to package jtoodle.api.test.web

---
+++ src/jtoodle/api/auth/AuthExceptionHandler.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jtoodle.api.auth;
+
+import java.awt.Component;
+import javax.swing.JOptionPane;
+import jtoodle.api.bean.util.JToodleException;
+
+/**
+ *
+ * @author Justo_Casablanca
+ */
+public class AuthExceptionHandler {
+
+	public static boolean handledInvalidKey( Component c, Exception ex ) {
+		boolean val = false;
+
+		if( ex instanceof JToodleException ) {
+			JToodleException jte = (JToodleException) ex;
+
+			if( jte.getErrorCode() == 2 ) {
+				// Error Code 2 = Invalid Key
+				AuthCache.logout();
+				JOptionPane.showMessageDialog( c, jte );
+				//loginMenuItemActionPerformed( null );
+				val = true;
+			}
+		}
+
+		return( val );
+	}
+
+}
