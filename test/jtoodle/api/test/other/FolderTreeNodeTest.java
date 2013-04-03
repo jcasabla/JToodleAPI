889aadeb5d33f8f62973814f5376216902da6f4d
Justo M. Casablanca <jcasablanca@bellsouth.net>
2013-04-03 14:51:52 -0400

    Just a quickie test class to learn how to navigate thru the Jackson JsonNode object.

---
+++ test/jtoodle/api/test/other/FolderTreeNodeTest.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jtoodle.api.test.other;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Iterator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import jtoodle.api.bean.core.Folder;
+import jtoodle.api.json.bean.ObjectMapperFactory;
+
+/**
+ *
+ * @author Justo_Casablanca
+ */
+public class FolderTreeNodeTest {
+
+	private static final Logger logger = Logger.getLogger( FolderTreeNodeTest.class.getName() );
+
+	public static void main( String[] args ) {
+		Folder f = new Folder();
+		f.setName( "My Folder Name" );
+		f.setOrder( 3 );
+		f.setPrivate( Boolean.TRUE );
+		f.setArchived( Boolean.FALSE );
+
+		ObjectMapper mapper = ObjectMapperFactory.getInstance();
+		JsonNode rootNode = mapper.valueToTree( f );
+
+		Iterator<String> fieldNames = rootNode.fieldNames();
+		for(int i=0; fieldNames.hasNext(); i++) {
+			String fName = fieldNames.next();
+			JsonNode fValue = rootNode.findValue( fName );
+
+			logger.log(  Level.INFO, "fieldNames[{0}] = [{1}], value = [{2}]",
+						 new Object[] { i, fName, fValue.asText() } );
+		}
+	}
+}
