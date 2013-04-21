ab8e510aa2b5114cae040f0fec8fd281ec08f3fc
Justo M. Casablanca <jcasablanca@bellsouth.net>
2013-04-21 12:52:17 -0400

    -

---
+++ src/jtoodle/api/request/web/TaskUpdateCriteria.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jtoodle.api.request.web;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import java.util.List;
+import jtoodle.api.bean.core.Task;
+import jtoodle.api.json.bean.BeanWriter;
+
+/**
+ *
+ * @author Justo_Casablanca
+ */
+public class TaskUpdateCriteria extends TaskOperations.OperationCriteria<Task> {
+
+	public TaskUpdateCriteria() {
+		super( Task.class, TaskOperations.OperationType.EDIT );
+		setOption( PARAM_EDIT_RESCHEDULE, "1" );
+		setOption( TaskSearchCriteria.PARAM_GET_TASKS_FIELD_LIST,
+				   TaskSearchCriteria.PARAM_VALUE_ALL_OPTIONAL_FIELDS );
+	}
+
+	public void setTasks( List<Task> tasksToEdit ) throws JsonProcessingException {
+		setOption( PARAM_EDIT_TASKS_TASK_LIST, BeanWriter.jsonString( tasksToEdit ) );
+	}
+
+	private static final String PARAM_EDIT_TASKS_TASK_LIST = "tasks";
+	private static final String PARAM_EDIT_RESCHEDULE = "reschedule";
+}
