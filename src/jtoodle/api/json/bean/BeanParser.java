444248f828843d2d6a8f19f9cf24667094f0cba2
Justo_Casablanca <jcasablanca@bellsouth.net>
2013-09-04 01:02:40 -0400

    Tons of source code reformatting using Netbeans 7.3.1. No functional changes, only code reformatting.

--- src/jtoodle/api/json/bean/BeanParser.java
+++ src/jtoodle/api/json/bean/BeanParser.java
@@ -22,16 +22,16 @@ public class BeanParser {
 	private BeanParser() {
 	}
 
-	public static <T extends AbstractJToodleBean> T parseBean( String js,
-															   Class<T> clazz )
-			throws IOException, JToodleException {
+	public static <T extends AbstractJToodleBean>
+		T parseBean( String js, Class<T> clazz )
+		throws IOException, JToodleException {
 		throwJToodleExceptionIfError( js );
 		return ObjectMapperFactory.getInstance().readValue( js, clazz );
 	}
 
-	public static <T extends AbstractJToodleBean> List<T> parseBeanList( 
-			String js, Class<T> clazz )
-			throws IOException, JToodleException {
+	public static <T extends AbstractJToodleBean> 
+		List<T> parseBeanList( String js, Class<T> clazz )
+		throws IOException, JToodleException {
 		throwJToodleExceptionIfError( js );
 		return ObjectMapperFactory.getInstance()
 					.readValue( js,
@@ -41,17 +41,17 @@ public class BeanParser {
 										clazz ) );
 	}
 
-	private static void throwJToodleExceptionIfError( String js ) throws
-			IOException, JToodleException {
+	private static void throwJToodleExceptionIfError( String js )
+		throws IOException, JToodleException {
 		if( js.contains( "errorCode" ) ) {
 			JToodleException jte = ObjectMapperFactory.getInstance().
-					readValue( js, JToodleException.class );
+				readValue( js, JToodleException.class );
 			throw ( jte );
 		}
 	}
 
 	public static TaskQueryResult parseTaskQueryResults( String js )
-			throws IOException, JToodleException {
+		throws IOException, JToodleException {
 		throwJToodleExceptionIfError( js );
 
 		TaskQueryResult results = null;
